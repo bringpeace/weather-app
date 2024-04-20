a34c3988b45f299045abb4273d60144171aef596
Shubhashini <shubhashiniverma@gmail.com>
2024-04-20 23:22:07 +0530

    first commit

---
+++ weatherTrack/src/app/environments/environment.ts
@@ -0,0 +1,10 @@
+export const environment = {
+    production :false,
+    apiKey: 'fb6cf7edf63d72f6e8fd475fa7dd5fc0',
+    dataUrl :`https://api.openweathermap.org/data/2.5/weather`,
+    airPollutionUrl :`http://api.openweathermap.org/data/2.5/air_pollution`,
+    cityToLat :`http://api.openweathermap.org/geo/1.0/direct`,
+
+
+
+};
